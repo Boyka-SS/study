36539f3842deb677bfccea89f05f00664a9a2dae
fanlihao <peanut_flh@163.com>
2023-05-31 07:15:34 +0800

    标准io 1
    Signed-off-by: fanlihao <peanut_flh@163.com>

--- note/linux/IO系统编程/index.c
+++ note/linux/IO系统编程/index.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+
 int main(int argc, char* argv[]) {
-  char buf[] = "hello linux";
-  int i = 0;
-  while (i < 95) {
-    printf("%s", buf);
-    i++;
+  char buf[] = "hello linux\n";
+  char read_buf[128] = {0};
+  FILE* fp = fopen("./a.c", "w+");
+  if (fp == NULL) {
+    printf("open a.c failure\n");
+    return -1;
   }
-  while (1)
-    ;
+  printf("open a.c success\n");
+  fputs(buf, fp);
+  fgets(read_buf, 128, fp);
+  printf("%s\n", read_buf);
+  fclose(fp);
   return 0;
 }
