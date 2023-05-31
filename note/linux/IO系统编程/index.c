c18ffe9b6e0bc2a50abcf420a0e07b2f8a5226e2
fanlihao <peanut_flh@163.com>
2023-05-31 23:06:07 +0800

    标准IO内容更新

--- note/linux/IO系统编程/index.c
+++ note/linux/IO系统编程/index.c
@@ -2,17 +2,5 @@
 #include <unistd.h>
 
 int main(int argc, char* argv[]) {
-  char buf[] = "hello linux\n";
-  char read_buf[128] = {0};
-  FILE* fp = fopen("./a.c", "w+");
-  if (fp == NULL) {
-    printf("open a.c failure\n");
-    return -1;
-  }
-  printf("open a.c success\n");
-  fputs(buf, fp);
-  fgets(read_buf, 128, fp);
-  printf("%s\n", read_buf);
-  fclose(fp);
-  return 0;
+  
 }
